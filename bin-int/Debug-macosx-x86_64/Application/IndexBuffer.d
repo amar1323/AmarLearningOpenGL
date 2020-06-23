f6b683fbdb1f8546849408b854668f330970a814
EL OUAZANI TUHAMI Amar <amar.et13@gmail.com>
2020-06-23 22:31:01 +0200

    remove intermidiate directory

--- bin-int/Debug-macosx-x86_64/Application/IndexBuffer.d
+++
@@ -1,9 +0,0 @@
-../bin-int/Debug-macosx-x86_64/Application/IndexBuffer.o: \
-  src/IndexBuffer.cpp src/IndexBuffer.h src/Renderer.h \
-  vendor/GLEW/include/GL/glew.h
-
-src/IndexBuffer.h:
-
-src/Renderer.h:
-
-vendor/GLEW/include/GL/glew.h:
--- bin-int/Debug-macosx-x86_64/Application/stb_image.d
+++
@@ -1,4 +0,0 @@
-../bin-int/Debug-macosx-x86_64/Application/stb_image.o: \
-  vendor/STB/stb_image.cpp vendor/STB/stb_image.h
-
-vendor/STB/stb_image.h:
